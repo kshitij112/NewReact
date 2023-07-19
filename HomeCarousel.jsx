8a5c279f74fae01109c568d6625f71c1945ad80f
Kshitij Patange <56216740+kshitij112@users.noreply.github.com>
2023-07-19 11:42:11 +0530

    Add files via upload

---
+++ HomeCarousel.jsx
@@ -0,0 +1,45 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import CarUI from "../../../images/slider/carui.png";
+import CarUI02 from "../../../images/slider/carui02.png";
+import CarUI03 from "../../../images/slider/carui03.png";
+import "./homecarousel.css";
+
+const HomeCarousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2400,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="mySlider">
+      <Slider {...settings}>
+        <div>
+          <img src={CarUI} alt="Slide 1" style={{ width: "100%" }} />
+        </div>
+        <div>
+          <img src={CarUI02} alt="Slide 2" style={{ width: "100%" }} />
+        </div>
+        <div>
+          <img src={CarUI03} alt="Slide 3" style={{ width: "100%" }} />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default HomeCarousel;
